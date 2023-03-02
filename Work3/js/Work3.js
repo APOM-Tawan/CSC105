d043e2cd3b5ccb9363a3d72e1f9223108c482ecf
APOM-Tawan <pathompong.cham@mail.kmutt.ac.th>
2023-03-02 11:58:57 +0700

    Work4

---
+++ Work3/js/Work3.js
@@ -0,0 +1,19 @@
+// var keys = "",  values = "";
+// var booklist = new Array("Chainese", "English", "Japan");
+// booklist.newField1 = "Something";
+
+// for(var key in booklist){
+//     keys += key +" ";
+//     values += booklist[counter]+" ";
+// }
+
+// var obj =  new Object(); 
+// obj.prop1 = 123;
+// obj.prop2 = "456";
+// obj["prop3"] = true;
+
+// var keys = "", values = "";
+// for(var p in obj){
+//     keys += p + " ";
+//     values += obj[p] + " ";
+// }
